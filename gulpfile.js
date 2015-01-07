14901af4778558cf137be51cc953103ee8e46e38
tschaef <tschaef1@gwdg.de>
2015-01-07 17:02:28 +0100

    [WIP] Refactor Sass, clean up templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,26 @@
-var gulp = require('gulp'),
-	livereload = require('gulp-livereload'),
-	compass = require('gulp-compass');
-
 var paths = {
 	sass: ['./Resources/Private/Scss/**/*.scss']
 };
 
-gulp.task('default', function() {
+var gulp = require('gulp'),
+	livereload = require('gulp-livereload'),
+	sourcemaps = require('gulp-sourcemaps'),
+	sass = require('gulp-sass')
 
+gulp.task('sass', function() {
+	gulp.src('./Resources/Private/Scss/digizeit.scss')
+		//.pipe(sourcemaps.init())
+		.pipe(sass())
+		//.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./Resources/Public/Css/'))
 });
-
-
-
-gulp.task('compass', function() {
-	gulp.src('./Resources/Private/Scss/**/*.scss')
-		.pipe(compass({
-						  css: './Resources/Public/Css',
-						  sass: './Resources/Private/Scss',
-						  image: './Resources/Public/Images'
-					  }))
-		.pipe(gulp.dest('./Resources/Public/Css'));
-});
-
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['compass']);
+	gulp.watch(paths.sass, ['sass'])
+});
+
+gulp.task('default', function() {
+	gulp.start('sass')
+	gulp.start('watch')
 });

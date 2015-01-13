63101a00b836de906325a65e1325777bc3381e9b
tschaef <tschaef1@gwdg.de>
2015-01-13 19:15:34 +0100

    [WIP] Lint all teh Sass!
    - Added scss-lint config (some checks disabled for now)
    - Rewrote gruntfile.js to execute linter and display a popup on compile errors
    - Renamed and cleaned up all SCSS variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,41 @@
-var paths = {
-	sass: ['./Resources/Private/Scss/**/*.scss']
-};
-
 var gulp = require('gulp'),
-	livereload = require('gulp-livereload'),
-	sourcemaps = require('gulp-sourcemaps'),
-	sass = require('gulp-sass')
+	//livereload = require('gulp-livereload'),
+	notify = require('gulp-notify'),
+	sass = require('gulp-ruby-sass'),
+	scsslint = require('gulp-scss-lint')
+
+var paths = {
+	sass: ['./Resources/Private/Scss/**/*.scss', '!./Resources/Private/Scss/vendors/**/*.scss']
+}
 
 gulp.task('sass', function() {
-	gulp.src('./Resources/Private/Scss/digizeit.scss')
-		//.pipe(sourcemaps.init())
-		.pipe(sass())
-		//.pipe(sourcemaps.write())
+	gulp.src(paths.sass)
+		.pipe(sass({
+			style: 'compressed'
+		}))
+		.on('error', notify.onError({
+			title: 'Sass Error',
+			message: '<%= error.message %>'
+		}))
 		.pipe(gulp.dest('./Resources/Public/Css/'))
-});
+})
 
-// Rerun the task when a file changes
+gulp.task('lint', function() {
+	gulp.src(paths.sass)
+		.pipe(scsslint({
+			'config': 'scss-lint.yml',
+			'maxBuffer': 9999999
+		}))
+})
+
+gulp.task('compile', function() {
+	gulp.start('sass')
+})
+
 gulp.task('watch', function() {
-	gulp.watch(paths.sass, ['sass'])
-});
+	gulp.watch(paths.sass, ['lint', 'compile'])
+})
 
 gulp.task('default', function() {
-	gulp.start('sass')
-	gulp.start('watch')
-});
+	gulp.start('lint', 'compile', 'watch')
+})

84bf634b0bd436c423657d6837cf41e94ae649e9
tschaef <tschaef1@gwdg.de>
2015-02-13 19:33:59 +0100

    [TASK] Style list content and MyDigiZeit
    - Harmonize margins and typography once again
    - Update gulpfile and package.json for a better build workflow
    - Update Goobit3 template
    
    Requires changes to FE Manager templates which are not part of this repo.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,41 @@
 var gulp = require('gulp'),
-	//livereload = require('gulp-livereload'),
-	notify = require('gulp-notify'),
-	sass = require('gulp-ruby-sass'),
-	scsslint = require('gulp-scss-lint')
+	autoprefixer = require('gulp-autoprefixer'),
+	notifier = require('node-notifier'),
+	sass = require('gulp-sass'),
+	scsslint = require('gulp-scss-lint'),
+	sourcemaps = require('gulp-sourcemaps')
 
-var paths = {
-	sass: ['./Resources/Private/Scss/**/*.scss', '!./Resources/Private/Scss/vendors/**/*.scss']
+var config = {
+	paths: {
+		sass: ['./Resources/Private/Scss/**/*.scss', '!./Resources/Private/Scss/vendors/**/*.scss'],
+		css: './Resources/Public/Css/'
+	},
+	dist: false
 }
 
 gulp.task('sass', function() {
-	gulp.src(paths.sass)
-		.pipe(sass({
-			style: 'compressed'
-		}))
-		.on('error', notify.onError({
-			title: 'Sass Error',
-			message: '<%= error.message %>'
-		}))
-		.pipe(gulp.dest('./Resources/Public/Css/'))
+	if ( config.dist ) {
+		gulp.src(config.paths.sass)
+			.pipe(sass({
+				outputStyle: 'compressed',
+				onError: function(error) { errorHandler('Sass', error) }
+			}))
+			.pipe(autoprefixer())
+			.pipe(gulp.dest(config.paths.css))
+	} else {
+		gulp.src(config.paths.sass)
+			.pipe(sourcemaps.init())
+			.pipe(sass({
+				onError: function(error) { errorHandler('Sass', error) }
+			}))
+			//.pipe(autoprefixer()) // TODO: .map file is generated, but only works if autoprefixer is disabled. Why?
+			.pipe(sourcemaps.write('.'))
+			.pipe(gulp.dest(config.paths.css))
+	}
 })
 
 gulp.task('lint', function() {
-	gulp.src(paths.sass)
+	gulp.src(config.paths.sass)
 		.pipe(scsslint({
 			'config': 'scss-lint.yml',
 			'maxBuffer': 9999999
@@ -33,9 +47,34 @@ gulp.task('compile', function() {
 })
 
 gulp.task('watch', function() {
-	gulp.watch(paths.sass, ['compile'])
+	gulp.watch(config.paths.sass, ['compile'])
+})
+
+gulp.task('dist', function() {
+	config.dist = true
+	gulp.start('lint', 'compile')
 })
 
 gulp.task('default', function() {
 	gulp.start('lint', 'compile', 'watch')
 })
+
+function errorHandler(title, error){
+	if (typeof error.file === 'undefined') error.file = '?'
+	if (typeof error.line === 'undefined') error.line = '?'
+	if (typeof error.column === 'undefined') error.column = '?'
+	notifier.notify({
+		title: title + ' Error',
+		message: "\n" + error.message + "\n\t\nFile: " + error.file + "\nLine: " + error.line + "\nColumn: " + error.column
+	});
+	console.log(color(title + ' Error: ' + error.message, 'red') + "\nFile: " + error.file + "\nLine: " + error.line + "\nColumn: " + error.column);
+}
+
+function color(string, color) {
+	var prefix
+	switch (color) {
+		case 'red': prefix = '\033[1;31m'; break; // and bold
+		case 'green': prefix = '\033[0;32m'; break;
+	}
+	return prefix + string + '\033[0m'
+}

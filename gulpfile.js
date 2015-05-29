37d036e133982b42d97dad17ef0e2143b9b4f54d
tschaef <tschaef1@gwdg.de>
2015-05-29 17:43:23 +0200

    Add gulp-svgmin and optimize SVG images

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,73 @@
+var config = {
+	paths: {
+		sass: ['./Resources/Private/Scss/**/*.scss', '!./Resources/Private/Scss/vendors/**/*.scss'],
+		css: './Resources/Public/Css/',
+		svg: './Resources/Public/Images/Layout/*.svg',
+		svgout: './Resources/Public/Images/Layout/'
+	},
+	production: false
+}
+
 var gulp = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),
 	livereload = require('gulp-livereload'),
 	notifier = require('node-notifier'),
 	sass = require('gulp-sass'),
 	scsslint = require('gulp-scss-lint'),
-	sourcemaps = require('gulp-sourcemaps')
+	sourcemaps = require('gulp-sourcemaps'),
+	svgmin = require('gulp-svgmin')
 
-var config = {
-	paths: {
-		sass: ['./Resources/Private/Scss/**/*.scss', '!./Resources/Private/Scss/vendors/**/*.scss'],
-		css: './Resources/Public/Css/'
-	},
-	production: false
+function errorHandler(title, error){
+	if (typeof error.file === 'undefined') error.file = '?'
+	if (typeof error.line === 'undefined') error.line = '?'
+	if (typeof error.column === 'undefined') error.column = '?'
+	notifier.notify({
+		title: title + ' Error',
+		message: "\n" + error.message + "\n\t\nFile: " + error.file + "\nLine: " + error.line + "\nColumn: " + error.column
+	});
+	console.log(color(title + ' Error: ' + error.message, 'red') + "\nFile: " + error.file + "\nLine: " + error.line + "\nColumn: " + error.column);
 }
+
+function color(string, color) {
+	var prefix
+	switch (color) {
+		case 'red': prefix = '\033[1;31m'; break; // and bold
+		case 'green': prefix = '\033[0;32m'; break;
+	}
+	return prefix + string + '\033[0m'
+}
+
+gulp.task('compile', function() {
+	gulp.start('sass')
+})
+
+gulp.task('default', function() {
+	gulp.start('lint', 'compile', 'watch')
+})
+
+gulp.task('lint', function() {
+	gulp.src(config.paths.sass)
+		.pipe(scsslint({
+			'config': 'scss-lint.yml',
+			'maxBuffer': 9999999
+		}))
+})
+
+gulp.task('lint-watch', function() {
+	gulp.start('lint')
+	gulp.watch(config.paths.sass, ['lint'])
+})
+
+gulp.task('production', function() {
+	config.production = true
+	gulp.start('lint', 'compile')
+})
+
+gulp.task('svgmin', function() {
+	return gulp.src(config.paths.svg)
+		.pipe(svgmin())
+		.pipe(gulp.dest(config.paths.svgout))
+})
 
 gulp.task('sass', function() {
 	if ( config.production ) {
@@ -38,53 +93,7 @@ gulp.task('sass', function() {
 	}
 })
 
-gulp.task('lint', function() {
-	gulp.src(config.paths.sass)
-		.pipe(scsslint({
-			'config': 'scss-lint.yml',
-			'maxBuffer': 9999999
-		}))
-})
-
-gulp.task('lint-watch', function() {
-	gulp.start('lint')
-	gulp.watch(config.paths.sass, ['lint'])
-})
-
-gulp.task('compile', function() {
-	gulp.start('sass')
-})
-
 gulp.task('watch', function() {
 	livereload.listen()
 	gulp.watch(config.paths.sass, ['compile'])
 })
-
-gulp.task('production', function() {
-	config.production = true
-	gulp.start('lint', 'compile')
-})
-
-gulp.task('default', function() {
-	gulp.start('lint', 'compile', 'watch')
-})
-
-function errorHandler(title, error){
-	if (typeof error.file === 'undefined') error.file = '?'
-	if (typeof error.line === 'undefined') error.line = '?'
-	if (typeof error.column === 'undefined') error.column = '?'
-	notifier.notify({
-		title: title + ' Error',
-		message: "\n" + error.message + "\n\t\nFile: " + error.file + "\nLine: " + error.line + "\nColumn: " + error.column
-	});
-	console.log(color(title + ' Error: ' + error.message, 'red') + "\nFile: " + error.file + "\nLine: " + error.line + "\nColumn: " + error.column);
-}
-
-function color(string, color) {
-	var prefix
-	switch (color) {
-		case 'red': prefix = '\033[1;31m'; break; // and bold
-		case 'green': prefix = '\033[0;32m'; break;
-	}
-	return prefix + string + '\033[0m'
-}

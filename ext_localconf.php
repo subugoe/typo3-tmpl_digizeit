986b5f3cb419403755bf249d5eddef8deeb761e4
Jochen Kothe <kothe@sub.uni-goettingen.de>
2015-06-19 15:25:00 +0200

    [TASK] Add DrwikiRealurlHelper to ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,9 @@
 <?php
 
 // TODO : ADD Documentation
+// makes the DrwikiRealurlHelper accessible via ?eID=realurlhelper
+$TYPO3_CONF_VARS['FE']['eID_include']['realurlhelper'] = 'EXT:tmpl_digizeit/Classes/Utility/DrwikiRealurlHelper.php';
+
 // makes the ipcalculator accessible via ?eID=ipcalc
 $TYPO3_CONF_VARS['FE']['eID_include']['ipcalc'] = 'EXT:tmpl_digizeit/Classes/Utility/IpCalculatorUtility.php';
 
